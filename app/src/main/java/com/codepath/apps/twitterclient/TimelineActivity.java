437b0b2b24be73e16e1fe8a0cd974ef61071aaba
Nurivan Gomez <nurivan@fb.com>
2022-06-07 11:05:19 -0700

    Added logging out

--- app/src/main/java/com/codepath/apps/twitterclient/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/twitterclient/TimelineActivity.java
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.codepath.apps.twitterclient.models.Tweet;
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
@@ -24,6 +27,7 @@ public class TimelineActivity extends AppCompatActivity {
     RecyclerView rvTweets;
     List<Tweet> tweetList;
     TweetsAdapter adapter;
+    Button bLogOut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +40,27 @@ public class TimelineActivity extends AppCompatActivity {
         adapter = new TweetsAdapter(this,  tweetList);
         rvTweets.setLayoutManager(new LinearLayoutManager(this));
         rvTweets.setAdapter(adapter);
+        bLogOut = findViewById(R.id.bLogOut);
+        bLogOut.setText("Log Out");
+        bLogOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onLogoutButton();
+            }
+        });
         populateHomeTimeline();
+    }
+
+    // TimelineActivity.java
+    private void onLogoutButton() {
+        // forget who's logged in
+        TwitterApp.getRestClient(this).clearAccessToken();
+
+        // navigate backwards to Login screen
+        Intent i = new Intent(this, LoginActivity.class);
+        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); // this makes sure the Back button won't work
+        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK); // same as above
+        startActivity(i);
     }
 
     private void populateHomeTimeline() {
